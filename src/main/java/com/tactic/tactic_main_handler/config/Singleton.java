d7c57d712d2ba4cf550c96d46e4014a10504f941
kapich1192 <kapich1192@yandex.ru>
2023-10-29 19:30:17 +0300

    feat: Добавил Синглетон с представлением приложения

---
+++ src/main/java/com/tactic/tactic_main_handler/config/Singleton.java
@@ -0,0 +1,7 @@
+package com.tactic.tactic_main_handler.config;
+
+import com.tactic.tactic_main_handler.models.dto.elements.Element;
+
+public class Singleton {
+    public static Element thisElement = null;
+}
