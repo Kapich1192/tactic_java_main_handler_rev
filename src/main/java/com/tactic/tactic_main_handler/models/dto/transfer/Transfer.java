503a3f69bfde21c638c9f93eb042a6008e8e4ab5
kapich1192 <kapich1192@yandex.ru>
2023-10-29 19:45:22 +0300

    feat: Добавил Трансферную модель

---
+++ src/main/java/com/tactic/tactic_main_handler/models/dto/transfer/Transfer.java
@@ -0,0 +1,10 @@
+package com.tactic.tactic_main_handler.models.dto.transfer;
+
+import com.tactic.tactic_main_handler.models.dto.devices.Device;
+
+import java.util.HashMap;
+
+public class Transfer {
+    private HashMap<String, Device> devices;
+    private HashMap<String, String> lifeDevice;
+}
