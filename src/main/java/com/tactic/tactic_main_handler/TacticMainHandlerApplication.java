29cf8d1e875bbaee319afc3d91a1e00687b67ebe
kapich1192 <kapich1192@yandex.ru>
2023-10-29 19:09:14 +0300

    init: First commit

---
+++ src/main/java/com/tactic/tactic_main_handler/TacticMainHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.tactic.tactic_main_handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacticMainHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacticMainHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tactic/tactic_main_handler/TacticMainHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tactic.tactic_main_handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacticMainHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
